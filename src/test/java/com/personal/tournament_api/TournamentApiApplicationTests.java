83a31d007c1e2bcfd73bdad9dfc7e9738be2c0e7
jotalvaro7 <jotalvaro.cc@gmail.com>
2025-10-28 12:31:40 -0500

    firts commit

---
+++ src/test/java/com/personal/tournament_api/TournamentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.tournament_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
